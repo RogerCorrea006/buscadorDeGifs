d2bcc3b762d5c7b2e971ae96b7cb59a5017457d8
RogerCorrea006 <roger.correa@beansoftwares.com.br>
2021-03-24 13:54:33 -0300

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:buscador_gif/ui/gif_page.dart';
+import 'package:flutter/material.dart';
+import 'package:buscador_gif/ui/homePage.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: HomePage(),
+    theme: ThemeData(hintColor: Colors.purple[100]),
+  ));
+}
